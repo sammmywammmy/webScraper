2854c6c65fa05b1a805425f847e434b103df5b37
sam <deedee123456654321@yahoo.co.uk>
2018-07-24 15:04:39 +0800

    working single h3 finder

--- htmlQuizScraper.py
+++ htmlQuizScraper.py
@@ -1,4 +1,11 @@
-import requests
+from requests import *
+from bs4 import BeautifulSoup
 
-response = requests.get("http://www.funtrivia.com/playquiz/quiz18138014c52e8.html")
-print(response.text)
+url = "https://www.horoscope.com/us/games/quiz/game-quiz-are-you-good-or-evil.aspx"
+getURL = get(url)
+getText = getURL.text
+soup = BeautifulSoup(getText, "html.parser")
+
+question = soup.select('h3')[0].text.strip()
+print(question)
+
