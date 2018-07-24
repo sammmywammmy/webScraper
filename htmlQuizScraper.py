f33efa98874596610731e045cca26062b4d7b040
sam <deedee123456654321@yahoo.co.uk>
2018-07-24 18:00:11 +0800

    working single q without tags

--- htmlQuizScraper.py
+++ htmlQuizScraper.py
@@ -1,11 +1,19 @@
-from requests import *
+import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.horoscope.com/us/games/quiz/game-quiz-are-you-good-or-evil.aspx"
-getURL = get(url)
-getText = getURL.text
-soup = BeautifulSoup(getText, "html.parser")
+def questions(max_pages):
+    page = 1
 
-question = soup.select('h3')[0].text.strip()
-print(question)
+    while page <= max_pages:
+        url = "https://www.horoscope.com/us/games/quiz/game-quiz-whats-in-your-immediate-future.aspx" + str(page)
+        getURL = requests.get(url, allow_redirects=False)
+        plain_text = getURL.text.encode('ascii', 'replace') #convert to plain text
+        soup = BeautifulSoup(plain_text, "html.parser") #parse plain text
+
+        question = soup.select('h3')
+        print(question)
+
+        page += 1
+
+questions(1)
 
--- multiQuestion.py
+++ multiQuestion.py
@@ -1,11 +1,18 @@
 from requests import *
 from bs4 import BeautifulSoup
+import re
 
 url = "https://www.horoscope.com/us/games/quiz/game-quiz-are-you-good-or-evil.aspx"
 getURL = get(url)
 getText = getURL.text
 plain_text = getURL.text.encode('ascii', 'replace')
 soup = BeautifulSoup(plain_text, "html.parser")
-question = soup.select('h3')[0].text.strip()
 
-print(question)
+question_containers = soup.findAll(['h3'])
+firstQuestion = question_containers[0]
+firstQ = firstQuestion.text
+print(firstQ)
+
+#for q in soup.findAll(["h3"]):
+  #  questions = q.
+   # print (questions)
