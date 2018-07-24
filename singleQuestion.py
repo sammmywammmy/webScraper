0ea4da9a4eda2ba6742d5d75854853f24c85e003
sam <deedee123456654321@yahoo.co.uk>
2018-07-24 16:18:00 +0800

    working single Q

---
+++ singleQuestion.py
@@ -0,0 +1,10 @@
+from requests import *
+from bs4 import BeautifulSoup
+
+url = "https://www.horoscope.com/us/games/quiz/game-quiz-are-you-good-or-evil.aspx"
+getURL = get(url)
+getText = getURL.text
+soup = BeautifulSoup(getText, "html.parser")
+
+question = soup.select('h3')[0].text.strip()
+print(question)
